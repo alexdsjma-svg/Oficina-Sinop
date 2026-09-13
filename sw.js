190df354fe6dd2ffe802b490320cadb8c76a3d65
alexdsjma-svg <alexdsj.ma@gmail.com>
2026-09-13 05:57:18 -0400

    Atualiza cache PWA para versão cloud

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'oficina-sinop-v6-2026-09-13';
-const CORE = ['./','./index.html','./manifest.json','./icon.svg'];
+const CACHE = 'oficina-sinop-v6-cloud-2026-09-13';
+const CORE = ['./','./index.html','./manifest.json','./icon.svg','./supabase-config.js','./cloud.js'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
