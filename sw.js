fb51f26b7de080e01253a235b0bb84dee98a8362
alexdsjma-svg <alexdsj.ma@gmail.com>
2026-09-13 05:32:23 -0400

    Adiciona sw.js ao PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'oficina-sinop-v6-2026-09-13';
+const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).then(res => {
+      const copy=res.clone(); caches.open(CACHE).then(cache=>cache.put('./index.html',copy)); return res;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    if (res && (res.ok || res.type === 'opaque')) {
+      const copy=res.clone(); caches.open(CACHE).then(cache=>cache.put(req,copy));
+    }
+    return res;
+  })));
+});
