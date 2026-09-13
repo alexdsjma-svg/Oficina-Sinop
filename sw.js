b298c5f5f6b86bac37cdcf3ba5421438dab89665
alexdsjma-svg <alexdsj.ma@gmail.com>
2026-09-13 13:53:46 -0400

    V11: força atualização do novo logo e banner

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ajc-oficina-v10-2-face-2026-09-13';
-const CORE = ['./','./index.html','./manifest.json','./ajc-icon-v10.svg','./ajc-banner-v10.svg','./ajc-splash-v10.svg','./supabase-config.js','./cloud.js'];
+const CACHE = 'ajc-oficina-v11-identidade-aprovada-2026-09-13';
+const CORE = ['./','./index.html','./manifest.json','./ajc-icon-v11.svg','./ajc-banner-v11.svg','./supabase-config.js','./cloud.js'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
