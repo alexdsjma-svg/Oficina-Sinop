ae34f4f336b0a080bd76137194116fdb9fdb14ca
alexdsjma-svg <alexdsj.ma@gmail.com>
2026-09-14 19:50:39 -0400

    Publica V18.7 isolada com Home V17.7 restaurada e instalador universal

---
+++ v18.7/sw.js
@@ -0,0 +1,5 @@
+const CACHE='ajc-oficina-v18-7-home-v177-restaurada-2';
+const CORE=['./','./index.html','./manifest.webmanifest','./ajc-icon-192.png','./ajc-icon-512.png','./mobile-v186.css','./realtime-v186.js','./home-v177-final-v187.css','./home-v177-final-v187.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{const req=e.request;if(req.method!=='GET')return;const url=new URL(req.url);if(req.mode==='navigate'){e.respondWith(fetch(req,{cache:'no-store'}).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return res}).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./'))));return}if(url.origin===self.location.origin)e.respondWith(fetch(req,{cache:'no-store'}).then(res=>{if(res&&res.ok){const cp=res.clone();caches.open(CACHE).then(c=>c.put(req,cp))}return res}).catch(()=>caches.match(req)))});
